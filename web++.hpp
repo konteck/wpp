43ce826ef9b59e889abc7651d3ac58fe80b855c4
Dany Henriquez <dany.henrique@vaartland.nl>
2020-01-15 10:34:31 +0100

    Minor adjustments to code and callbacks  as well as void pointer

--- web++.hpp
+++ web++.hpp
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <sstream>
 #include <regex>
+#include <functional>
 
 #define SERVER_NAME "Web++"
 #define SERVER_VERSION "1.0.1"
@@ -65,6 +66,8 @@ namespace WPP {
             };
         private:
     };
+
+    using callbackType = function<void(Request*, Response*)>;
 
     class Exception : public std::exception {
         public:
@@ -276,7 +279,7 @@ namespace WPP {
     struct Route {
         string path;
         string method;
-        void (*callback)(Request*, Response*);
+        callbackType callback;
         string params;
     };
 
@@ -284,9 +287,9 @@ namespace WPP {
 
     class Server {
         public:
-            void get(string, void (*callback)(Request*, Response*));
-            void post(string, void (*callback)(Request*, Response*));
-            void all(string, void (*callback)(Request*, Response*));
+            void get(string, callbackType);
+            void post(string, callbackType);
+            void all(string, callbackType);
             void get(string, string);
             void post(string, string);
             void all(string, string);
@@ -390,7 +393,7 @@ namespace WPP {
         }
     }
 
-    void Server::get(string path, void (*callback)(Request*, Response*)) {
+    void Server::get(string path, callbackType callback) {
         Route r = {
              path,
              "GET",
@@ -400,7 +403,7 @@ namespace WPP {
         ROUTES.push_back(r);
     }
 
-    void Server::post(string path, void (*callback)(Request*, Response*)) {
+    void Server::post(string path, callbackType callback) {
         Route r = {
              path,
              "POST",
@@ -410,7 +413,7 @@ namespace WPP {
         ROUTES.push_back(r);
     }
 
-    void Server::all(string path, void (*callback)(Request*, Response*)) {
+     void Server::all(string path, callbackType callback) {
         Route r = {
              path,
              "ALL",
@@ -523,7 +526,7 @@ namespace WPP {
 
             char header_buffer[BUFSIZE];
             string body = res.body.str();
-            size_t body_len = strlen(body.c_str());
+            size_t body_len = body.size();
 
             // build http response
             sprintf(header_buffer, "HTTP/1.0 %d %s\r\n", res.code, res.phrase.c_str());
